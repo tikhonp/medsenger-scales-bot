46b95ec029420f4a67b352d336f1235ad5f8d475
TikhonP <tikhon.petrishchev@gmail.com>
2024-09-30 22:52:03 +0300

    Added sendrecord call

--- handler/new_record.go
+++ handler/new_record.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/TikhonP/maigo"
 	"github.com/labstack/echo/v4"
@@ -9,8 +12,9 @@ import (
 )
 
 type newRecordModel struct {
-	AgentToken string `json:"agent_token" validate:"required"`
-	Weight     int    `json:"weight" validate:"required"`
+	AgentToken string    `json:"agent_token" validate:"required"`
+	Weight     int       `json:"weight" validate:"required"`
+	Time       time.Time `json:"time" validate:"required"`
 }
 
 type NewRecord struct {
@@ -30,6 +34,10 @@ func (h NewRecord) Handle(c echo.Context) error {
 		return err
 	}
 	go func() {
+		_, err := h.MaigoClient.AddRecord(contract.Id, "weight", fmt.Sprint(m.Weight), m.Time, nil)
+		if err != nil {
+			log.Println("Failed to send record to maigo:", err)
+		}
 		// TODO: Send record to maigo
 		// so i need to implement it in maigo first :(
 	}()
